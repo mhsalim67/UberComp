d4a184d646e6c7f6102a1de8e26d75d5d537b0b3
Geoff Miller <miller.geoffw@gmail.com>
2013-07-02 22:04:06 -0300

    Add more error catching.

--- mobile/upload.php
+++ mobile/upload.php
@@ -1,29 +1,36 @@
 <?php
 	require('SimpleImage.php');
 
-	$return = array(
+	$response = array(
 		'success' => false,
 		'feedback' => 'Sorry, there was an unexpected error'
 	);
 
 	if($file = $_FILES['file']) {
-		$filepath = '../common/images/';
-		$time = time();
+		if($file['error'] > 0) {
+			$response['feedback'] = 'Sorry, there was an upload error';
+		} else {
+			$filepath = '../common/images/';
+			$time = time();
 
-		move_uploaded_file($file['tmp_name'], $filepath.$time);
-		$image = new SimpleImage(); 
-		$image->load($filepath.$time); 
-		$image->resizeToWidth(250); 
-		$image->save($filepath.$time); 
+			if(move_uploaded_file($file['tmp_name'], $filepath.$time)) {
+				$image = new SimpleImage(); 
+				$image->load($filepath.$time); 
+				$image->resizeToWidth(250); 
+				$image->save($filepath.$time); 
 
-		$return['img_src'] = $filepath.$time;
-		$return['image_name'] = $_POST['image_name'];
-		$return ['image_comments'] = $_POST['image_comments'];
-		$return['feedback'] = 'Your image was uploaded successfully';
-		$return['success'] = true;
+				$response['img_src'] = $filepath.$time;
+				$response['image_name'] = $_POST['image_name'];
+				$response['image_comments'] = $_POST['image_comments'];
+				$response['feedback'] = 'Your image was uploaded successfully';
+				$response['success'] = true;
+			} else {
+				$response['feedback'] = 'Sorry, there was an error saving your image';
+			}
+		}
 	} else {
-		$return['feedback'] = 'You need to upload a file';
+		$response['feedback'] = 'You need to upload a file';
 	}
-	echo json_encode($return);
+	echo json_encode($response);
 
 ?>
