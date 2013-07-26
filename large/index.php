b508eb0b7458a4a0182197e2ad27673db4cdac9c
mhsalim67 <salimian@live.ca>
2013-07-26 12:51:49 -0300

    comments are added

--- large/index.php
+++ large/index.php
@@ -15,14 +15,21 @@
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false"></script>
 	<link rel="stylesheet" type="text/css" href="Style/mystyle.css" />
 	<script>
+		var fake_coordinates = 0;
+		<!-- Fake coordinates just for the userstudy -->
+		var fake_coordinates_array =[[44.647418, -63.580434],[44.624896, -63.571014],[44.625647, -63.604145],[44.648366, -63.621483],[44.639276,-63.589725]];
 		var map;
+		<!-- Counter to keep 10 items on the map -->
 		var counter =1;
+		<!-- In the first run when there is no items on the map, it wont let the program to try remove items that there are not on them; so there wont be an error -->
 		var stat = 0;
+		<!--AN ararray of markers to place on the map  -->
 		var marker = [];
 		var infowindow;
+		<!--Defining map options such as: zoom level, center of the map, type of map-->
 		function initialize() {
 		  var mapOptions = {
-			zoom: 13,
+			zoom: 14,
 			center: new google.maps.LatLng(44.636672,-63.591421),
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		  };
@@ -30,15 +37,24 @@
 			  mapOptions);
 
 		}
+		<!--every time user send an item to the map, this function will be called to add the image as a marker to the map  -->
+		<!--dal.png is the an ivisible marker, and side defined the size of popout windows on the map -->
 function addIcons(response){
 	var image = {
     url: 'img/dal.png',
     size: new google.maps.Size(50, 47),
   };
-  var myLatLng = new google.maps.LatLng(response.position.latitude,response.position.longitude);
-  console.log(counter);
+    var myLatLng;
+			<!--this is function we added for fake items during users study, only first 4 iamges use the fake coordinate, after that it use user real coordinate, By changing 4 to -1 we always have the real coordinate -->
+  if(fake_coordinates <4){
+     myLatLng = new google.maps.LatLng(fake_coordinates_array[fake_coordinates][0],fake_coordinates_array[fake_coordinates][1]);
+		 fake_coordinates++;
+	}else{
+	     myLatLng = new google.maps.LatLng(fake_coordinates_array[4][0],fake_coordinates_array[4][1]);
+
+	}
+			<!-- having 10 items on the map -->
   if (counter == 10){
-  
 	counter = 1;
 	stat = 1;
   }
@@ -51,22 +67,25 @@ function addIcons(response){
 		title: response.img_name,
         map: map
 		});
-		
-		map.setCenter(myLatLng);
+	<!--Defining html style for the pop out windows  -->
+
 		var contentString = '<div id="content"  ><table border="0" > <tbody><tr><td rowspan="2" width="100" height="62"><img src="'+
 	  config.image_location+response.img_src+config.small_image+'" /></td><td width="50" height="12">'+response.img_name+'</td></tr><tr><td width="50" height="50">'+
       '<img src="'+config.image_qrcode+response.img_src+
 	  '"alt="qr code" width="50" height="50"  align="top"/></td></tr><tr><td colspan="2" width="200" ><div class="your-selector ">'+
       response.comments+'</div></td></tr></tbody></table></div>';
+	<!-- adding the pop out windows th the marker, this windows has the name, comments and the image -->
 
    marker[counter].infowindow = new google.maps.InfoWindow({
       content: contentString,
 	  maxWidth: 200
 	  });
-
+	  
+		<!--for the touch screen display, users can click on the map to have more windows  -->
   google.maps.event.addListener(marker[counter], 'click', function() {
 	marker[counter].infowindow.open(map,marker);	
   });		
+  		<!--windows will be open by defult in non touch screen displays  -->
   marker[counter].infowindow.open(map,marker[counter]);	
 counter ++;  
 }
